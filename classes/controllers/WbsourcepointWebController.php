8dc0cd1b37a39397fb154bc1074fb810ee6d2b72
Michael H. Arieli <excelwebzone@gmail.com>
2015-09-21 11:47:51 -0700

    RE #13: Controller cleanup
    - Use defined variables
    - Assume $distributedCacheStore exists
    - Set cache to null of failure (store for 1 hour)

--- classes/controllers/WbsourcepointWebController.php
+++ classes/controllers/WbsourcepointWebController.php
@@ -6,17 +6,20 @@
 class WbsourcepointWebController extends NodeWebController
 {
     /** @var \CacheStoreInterface */
-    protected $DistributedCacheStore;
+    protected $distributedCacheStore;
 
     /** @var int */
     protected $cacheTtl = 86400; //24 hours
 
+    /** @var int */
+    protected $failedCacheTtl = 3600; //! hours
+
     /**
-     * @param \CacheStoreInterface $DistributedCacheStore
+     * @param \CacheStoreInterface $val
      */
-    public function setDistributedCacheStore(\CacheStoreInterface $DistributedCacheStore)
+    public function setDistributedCacheStore(\CacheStoreInterface $val)
     {
-        $this->DistributedCacheStore = $DistributedCacheStore;
+        $this->distributedCacheStore = $val;
     }
 
     /**
@@ -33,20 +36,17 @@ class WbsourcepointWebController extends NodeWebController
             return ['data' => ['script' => null]];
         }
 
-        if ($this->DistributedCacheStore) {
-            $this->Logger->debug('looking in cache for script with cacheKey:' . $this->cacheKey());
-
-            if ($this->DistributedCacheStore->containsKey($this->cacheKey())) {
-                $cachedScript = $this->DistributedCacheStore->get($this->cacheKey());
-                $this->Logger->debug(sprintf('...found a match in DCS for cacheKey: %s', $this->cacheKey()));
-                return ['data' => ['script' => $cachedScript]];
-            }
+        $this->Logger->debug('looking in cache for script with cacheKey:' . $this->cacheKey());
+        if ($this->distributedCacheStore->containsKey($this->cacheKey())) {
+            $cachedScript = $this->distributedCacheStore->get($this->cacheKey());
+            $this->Logger->debug(sprintf('...found a match in DCS for cacheKey: %s', $this->cacheKey()));
+            return ['data' => ['script' => $cachedScript]];
         }
 
         try {
             $curl = curl_init();
-            curl_setopt($curl, CURLOPT_URL, sprintf('%sscript?delivery=script', $this->getRequiredTemplateVariable('ApiUrl')));
-            curl_setopt($curl, CURLOPT_HTTPHEADER, [sprintf('Authorization: Token %s', $this->getRequiredTemplateVariable('ApiKey'))]);
+            curl_setopt($curl, CURLOPT_URL, sprintf('%sscript?delivery=script', $apiUrl));
+            curl_setopt($curl, CURLOPT_HTTPHEADER, [sprintf('Authorization: Token %s', $apiKey)]);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 1);
             curl_setopt($curl, CURLOPT_TIMEOUT, 1);
@@ -57,16 +57,15 @@ class WbsourcepointWebController extends NodeWebController
             // ex: "<script async="async" data-client-id="RXcVfPPwlbdGjwq" type="text/javascript" src="//d3ujids68p6xmq.cloudfront.net/abw.js"></script>"
             if (preg_match('/<script(.*?)(\\/>|<\\/script>)/i', $script) !== false) {
                 $this->Logger->debug(sprintf('Found script?: %s', $script));
-
-                if ($this->DistributedCacheStore) {
-                    $this->Logger->debug(sprintf('Update that cacheKey "%s" with: %s', $this->cacheKey(), $script));
-                    $this->DistributedCacheStore->put($this->cacheKey(), $script, $this->cacheTtl);
-                }
+                $this->Logger->debug(sprintf('Update that cacheKey "%s" with: %s', $this->cacheKey(), $script));
+                $this->distributedCacheStore->put($this->cacheKey(), $script, $this->cacheTtl);
 
                 return ['data' => ['script' => $script]];
             }
         } catch (\Exception $e) {
-            // do nothing
+            $this->Logger->debug(sprintf('CURL failed with: %s', $e->getMessage()));
+            $this->Logger->debug(sprintf('Update that cacheKey "%s"  to null', $this->cacheKey()));
+            $this->distributedCacheStore->put($this->cacheKey(), null, $this->failedCacheTtl);
         }
 
         return ['data' => ['script' => null]];
