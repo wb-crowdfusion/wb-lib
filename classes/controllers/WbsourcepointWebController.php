d4427bbd7460d5728d89882cb144642c225e7a42
Greg Brown <greg@12dnetworks.com>
2015-09-25 16:48:48 -0700

    re #13 some minor cleanup and changed failed cache value to a friendly message so we can see it in code

--- classes/controllers/WbsourcepointWebController.php
+++ classes/controllers/WbsourcepointWebController.php
@@ -3,49 +3,59 @@
 /**
  * Provides functions for getting the sourcepoint js.
  */
-class WbsourcepointWebController extends NodeWebController
+class WbsourcepointWebController extends AbstractWebController
 {
     /** @var \CacheStoreInterface */
-    protected $distributedCacheStore;
+    protected $cacheStore;
 
-    /** @var int */
-    protected $cacheTtl = 86400; //24 hours
+    /**
+     * 24 hours
+     * @var int
+     */
+    protected $cacheTtl = 86400;
 
-    /** @var int */
-    protected $failedCacheTtl = 3600; //1 hour
+    /**
+     * 1 hour
+     * @var int
+     */
+    protected $failedCacheTtl = 3600;
 
     /**
      * @param \CacheStoreInterface $val
      */
     public function setDistributedCacheStore(\CacheStoreInterface $val)
     {
-        $this->distributedCacheStore = $val;
+        $this->cacheStore = $val;
     }
 
     /**
-     * Generates script url.
+     * Gets a script url from sourcepoint and caches it for a day.
      *
-     * @return string
+     * @return array
      */
-    public function getAsScript()
+    protected function getAsScript()
     {
-        $apiUrl = $this->getRequiredTemplateVariable('ApiUrl');
-        $apiKey = $this->getRequiredTemplateVariable('ApiKey');
+        $apiUrl = trim($this->getTemplateVariable('ApiUrl'));
+        $apiKey = trim($this->getTemplateVariable('ApiKey'));
 
-        if (!$apiUrl || !$apiKey) {
-            return ['data' => ['script' => null]];
+        $failedValue = '<!--sourcepoint-failed-->';
+        $cacheKey = 'wblib:sourcepoint:script';
+
+        if (empty($apiUrl) || empty($apiKey)) {
+            return ['data' => ['script' => $failedValue]];
         }
 
-        //$this->Logger->debug('looking in cache for script with cacheKey:' . $this->cacheKey());
-        if ($this->distributedCacheStore->containsKey($this->cacheKey())) {
-            $cachedScript = $this->distributedCacheStore->get($this->cacheKey());
-            //$this->Logger->debug(sprintf('...found a match in DCS for cacheKey: %s', $this->cacheKey()));
+        $cachedScript = $this->cacheStore->get($cacheKey);
+        if (false !== $cachedScript) {
             return ['data' => ['script' => $cachedScript]];
         }
 
+        $scriptEndpoint = sprintf('%sscript?delivery=script', $apiUrl);
+        $script = null;
+
         try {
             $curl = curl_init();
-            curl_setopt($curl, CURLOPT_URL, sprintf('%sscript?delivery=script', $apiUrl));
+            curl_setopt($curl, CURLOPT_URL, $scriptEndpoint);
             curl_setopt($curl, CURLOPT_HTTPHEADER, [sprintf('Authorization: Token %s', $apiKey)]);
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 1);
@@ -54,30 +64,18 @@ class WbsourcepointWebController extends NodeWebController
             curl_close($curl);
 
             // validate script tag
-            // ex: "<script async="async" data-client-id="RXcVfPPwlbdGjwq" type="text/javascript" src="//d3ujids68p6xmq.cloudfront.net/abw.js"></script>"
+            // e.g.: "<script async="async" data-client-id="RXcVfPPwlbdGjwq" type="text/javascript" src="//d3ujids68p6xmq.cloudfront.net/abw.js"></script>"
             if (preg_match('/<script(.*?)(\\/>|<\\/script>)/i', $script) !== false) {
-                //$this->Logger->debug(sprintf('Found script?: %s', $script));
-                //$this->Logger->debug(sprintf('Update that cacheKey "%s" with: %s', $this->cacheKey(), $script));
-                $this->distributedCacheStore->put($this->cacheKey(), $script, $this->cacheTtl);
-
-                return ['data' => ['script' => $script]];
+                $this->cacheStore->put($cacheKey, $script, $this->cacheTtl);
+            } else {
+                $script = $failedValue;
+                $this->cacheStore->put($cacheKey, $failedValue, $this->failedCacheTtl);
             }
         } catch (\Exception $e) {
-            $this->Logger->debug(sprintf('CURL failed with: %s', $e->getMessage()));
-            $this->Logger->debug(sprintf('Update that cacheKey "%s"  to null', $this->cacheKey()));
-            $this->distributedCacheStore->put($this->cacheKey(), null, $this->failedCacheTtl);
+            $this->Logger->error(sprintf('CURL to [%s] failed with: %s', $scriptEndpoint, $e->getMessage()));
+            $this->cacheStore->put($cacheKey, $failedValue, $this->failedCacheTtl);
         }
 
-        return ['data' => ['script' => null]];
-    }
-
-    /**
-     * Returns a namepsaced cache key
-     *
-     * @return string
-     */
-    private function cacheKey()
-    {
-        return 'wblib:aws:sourcepoint';
+        return ['data' => ['script' => $script]];
     }
 }
