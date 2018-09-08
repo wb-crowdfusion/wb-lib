972b0f1d4aa2fa538ee6c6b436ac84849a579326
Richard Sumilang <rsumilang@users.noreply.github.com>
2018-09-07 18:27:50 -0700

    Adding support for different sourcepoint delivery methods and a json … (#26)
    * Adding support for different sourcepoint delivery methods and a json response.
    
    * Updating changelog
    
    * - Updating plugin.xml
    - Better error checking via http code
    
    * Reverse capability

--- classes/controllers/WbsourcepointWebController.php
+++ classes/controllers/WbsourcepointWebController.php
@@ -29,17 +29,38 @@ class WbsourcepointWebController extends AbstractWebController
     }
 
     /**
+     * Please use getScript() going forward.
+     * @deprecated
+     */
+    public function getAsScript() {
+        return $this->getScript();
+    }
+
+    /**
      * Gets a script url from sourcepoint and caches it for a day.
+     *
+     * Accepts the following arguments:
+     *  delivery = script | inline | bundle
+     *  response = json
+     *
+     * Note: JSON response is only supported when delivery is set to script.
      *
      * @return array
      */
-    protected function getAsScript()
+    protected function getScript()
     {
         $apiUrl = trim($this->getTemplateVariable('api_url'));
         $apiKey = trim($this->getTemplateVariable('api_key'));
 
-        $failedValue = '<!--sourcepoint-failed-->';
-        $cacheKey = 'wblib:sourcepoint:script';
+        $delivery = $this->getTemplateVariable('delivery');
+        if (!$delivery) {
+            $delivery = 'script';
+        }
+        $response = $this->getTemplateVariable('response');
+
+        $failedValue = ($delivery === 'script' && $response === 'json') ?
+            '{} /* sourcepoint failed */' : '<!--sourcepoint-failed-->';
+        $cacheKey = sprintf('wblib:sourcepoint:script:%s:%s', $delivery, $response);
 
         if (empty($apiUrl) || empty($apiKey)) {
             return ['data' => ['script' => $failedValue]];
@@ -50,7 +71,7 @@ class WbsourcepointWebController extends AbstractWebController
             return ['data' => ['script' => $cachedScript]];
         }
 
-        $scriptEndpoint = sprintf('%sscript?delivery=script', $apiUrl);
+        $scriptEndpoint = sprintf('%sscript/detection?delivery=%s', $apiUrl, $delivery);
         $script = null;
 
         try {
@@ -60,15 +81,29 @@ class WbsourcepointWebController extends AbstractWebController
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 1);
             curl_setopt($curl, CURLOPT_TIMEOUT, 1);
+
             $script = trim(curl_exec($curl));
+            $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
             curl_close($curl);
 
-            // validate script tag
-            // e.g.: "<script async="async" data-client-id="RXcVfPPwlbdGjwq" type="text/javascript" src="//d3ujids68p6xmq.cloudfront.net/abw.js"></script>"
-            if (preg_match('/^<script/i', $script)) {
-                $this->cacheStore->put($cacheKey, $script, $this->cacheTtl);
+            if ((200 <= $httpCode) && ($httpCode < 400)) {
+                if ($delivery === 'script' && $response === 'json') {
+                    $el = new SimpleXmlElement($script);
+                    $attributes = (array) $el->attributes();
+                    $attributesArr = $attributes['@attributes'];
+                    $script = json_encode($attributesArr);
+                    $this->cacheStore->put($cacheKey, $script, $this->cacheTtl);
+                } else {
+                    // This can be a number of different formats
+                    // URL: https://analytics.sourcepoint.com/doc/index#get-script-as-tag-on-page
+                    // e.g.: <script async="async" data-client-id="YOUR_CLIENT_ID" type="text/javascript" src="//d3ujids68p6xmq.cloudfront.net/abw.js"></script>
+                    // e.g.: <script data-client-id="YOUR_CLIENT_ID" type="text/javascript">function detect() {};</script>
+                    // e.g.: function detect() {};
+                    $this->cacheStore->put($cacheKey, $script, $this->cacheTtl);
+                }
             } else {
-                $this->Logger->error(sprintf('Expected a script tag but got: %s', $script));
+                $this->Logger->error(sprintf('Received an HTTP error code: %s', $httpCode));
                 $script = $failedValue;
                 $this->cacheStore->put($cacheKey, $failedValue, $this->failedCacheTtl);
             }
